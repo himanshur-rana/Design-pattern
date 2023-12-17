562e443053c97518adbeb13c747784b5e4664900
Himanshu Rana <himanshurjj@gmail.com>
2023-12-17 15:15:34 +0530

    changes

--- src/singleton-pattern/client.ts
+++ src/singleton-pattern/client.ts
@@ -9,6 +9,9 @@ const main = () => {
 
   const instance3 = SingletonClass.getInstance();
   instance3.doSomeWork();
+
+  const instance4 = SingletonClass.getInstance();
+  instance4.doSomeWork();
 };
 
 main();
